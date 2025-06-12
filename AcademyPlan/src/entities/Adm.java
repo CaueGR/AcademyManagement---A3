4b35b3ce231cd06ad7c4f17346b32397aa3bf7a0
Kauã <kaua@asefs>
2025-06-11 23:16:04 -0300

    Classes e Interfaces Novas

--- AcademyPlan/src/entities/Adm.java
+++ AcademyPlan/src/entities/Adm.java
@@ -1,11 +1,44 @@
 package entities;
 
-public class Adm {
+import Interfaces.GerenciamentoPlano;
+
+public class Adm implements GerenciamentoPlano{
 
     private int numeroAcesso;
     private String senhaAdm;
 
+    
 
+    @Override
+    public void criarPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void criarPlanoAlimentar() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void editarPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void editarPlanoAlimentar() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void excluirPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void excluirPlanoAliementar() {
+        // TODO Auto-generated method stub
+        
+    }
 
 
     
--- AcademyPlan/src/entities/Treinador.java
+++ AcademyPlan/src/entities/Treinador.java
@@ -1,31 +1,85 @@
 package entities;
 
-public class Treinador {
+import java.time.LocalDate;
+
+import Interfaces.GerenciamentoPlano;
+
+public class Treinador extends Pessoa implements GerenciamentoPlano {
 
     private String especializacao;
     private String disponibilidade;
-    
-    public Treinador(String especializacao, String disponibilidade) {
+
+
+    public Treinador(String name, LocalDate dataNascimento, Integer telefone, Integer cpf, Integer cep,
+            String especializacao, String disponibilidade) {
+        super(name, dataNascimento, telefone, cpf, cep);
         this.especializacao = especializacao;
         this.disponibilidade = disponibilidade;
     }
+
     
+
     public String getEspecializacao() {
         return especializacao;
     }
+
+
+
     public void setEspecializacao(String especializacao) {
         this.especializacao = especializacao;
     }
+
+
+
     public String getDisponibilidade() {
         return disponibilidade;
     }
+
+
+
     public void setDisponibilidade(String disponibilidade) {
         this.disponibilidade = disponibilidade;
     }
+
+
+
     @Override
-    public String toString() {
-        return "Treinador [especializacao=" + especializacao + ", disponibilidade=" + disponibilidade + "]";
+    public void criarPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
     }
+
+    @Override
+    public void criarPlanoAlimentar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void editarPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void editarPlanoAlimentar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void excluirPlanoAcademia() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void excluirPlanoAliementar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
 
     
 
